fffd50531e898031e8a3591858bcb53681f31efb
Pipos <marnei.machado@gmail.com>
2020-08-21 20:32:17 -0300

    Applying pair
    Checking if forming pair

--- scripts/script.js
+++ scripts/script.js
@@ -44,8 +44,20 @@ function createCardFace(face, card, element) {
   element.appendChild(cardElementFace);
 }
 
-createCardsFromTechs(techs);
-
 function flipCard() {
-  this.classList.add("flip");
+  if (game.setCard(this.id)) {
+    this.classList.add("flip");
+    if (game.checkMatch()) {
+      game.clearCards();
+    } else {
+      setTimeout(() => {
+        let firstCardView = document.getElementById(game.firstCard.id);
+        let secondCardView = document.getElementById(game.secondCard.id);
+
+        firstCardView.classList.remove("flip");
+        secondCardView.classList.remove("flip");
+        game.clearCards();
+      }, 1000);
+    }
+  }
 }
