40e52773a2489e2ea2d51b5b92f4c9b5cbb6c8b8
Pipos <marnei.machado@gmail.com>
2020-08-23 21:21:07 -0300

    Finished
    Game corrected and finished

--- scripts/game.js
+++ scripts/game.js
@@ -2,32 +2,6 @@ let game = {
   lockMode: false,
   firstCard: null,
   secondCard: null,
-
-  setCard: function (id) {
-    let card = this.cards.filter((card) => card.id === id)[0];
-    if (card.flipped || this.lockMode) {
-      return false;
-    }
-    if (!this.firstCard) {
-      this.firstCard = card;
-      return true;
-    } else {
-      this.secondCard = card;
-      this.lockMode = true;
-      return true;
-    }
-  },
-
-  checkMatch: function () {
-    return this.firstCard.icon === this.secondCard.icon;
-  },
-
-  clearCards: function () {
-    this.firstCard = null;
-    this.secondCard = null;
-    this.lockMode = false;
-  },
-
   techs: [
     "bootstrap",
     "css",
@@ -40,6 +14,45 @@ let game = {
     "node",
     "react",
   ],
+
+  setCard: function (id) {
+    let card = this.cards.filter((card) => card.id === id)[0];
+    console.log(card);
+    if (card.flipped || this.lockMode) {
+      return false;
+    }
+    if (!this.firstCard) {
+      this.firstCard = card;
+      this.firstCard.flipped = true;
+      return true;
+    } else {
+      this.secondCard = card;
+      this.secondCard.flipped = true;
+      this.lockMode = true;
+      return true;
+    }
+  },
+
+  checkMatch: function () {
+    if (!this.firstCard || !this.secondCard) {
+      return false;
+    }
+    return this.firstCard.icon === this.secondCard.icon;
+  },
+
+  clearCards: function () {
+    this.firstCard = null;
+    this.secondCard = null;
+    this.lockMode = false;
+  },
+  unflipCards() {
+    this.firstCard.flipped = false;
+    this.secondCard.flipped = false;
+    this.clearCards();
+  },
+  checkGameOver() {
+    return this.cards.filter((card) => !card.flipped).length == 0;
+  },
 
   cards: null,
 
--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,7 @@ function startGame() {
 
 function initializeCards(cards) {
   let gameBoard = document.getElementById("gameBoard");
+  gameBoard.innerHTML = "";
 
   game.cards.forEach((card) => {
     let cardElement = document.createElement("div");
@@ -47,17 +48,30 @@ function createCardFace(face, card, element) {
 function flipCard() {
   if (game.setCard(this.id)) {
     this.classList.add("flip");
-    if (game.checkMatch()) {
-      game.clearCards();
-    } else {
-      setTimeout(() => {
-        let firstCardView = document.getElementById(game.firstCard.id);
-        let secondCardView = document.getElementById(game.secondCard.id);
-
-        firstCardView.classList.remove("flip");
-        secondCardView.classList.remove("flip");
+    if (game.secondCard) {
+      if (game.checkMatch()) {
         game.clearCards();
-      }, 1000);
+        if (game.checkGameOver()) {
+          let gameOverLayer = document.getElementById("gameOver");
+          gameOverLayer.style.display = "flex";
+        }
+      } else {
+        setTimeout(() => {
+          let firstCardView = document.getElementById(game.firstCard.id);
+          let secondCardView = document.getElementById(game.secondCard.id);
+
+          firstCardView.classList.remove("flip");
+          secondCardView.classList.remove("flip");
+          game.unflipCards();
+        }, 1000);
+      }
     }
   }
+}
+
+function restart() {
+  game.clearCards();
+  startGame();
+  let gameOverLayer = document.getElementById("gameOver");
+  gameOverLayer.style.display = "none";
 }
